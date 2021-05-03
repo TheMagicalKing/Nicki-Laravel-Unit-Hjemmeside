ad9d45fc5dc3fc5fa473eadc906d9ede089baff6
Nicki <alternativelenicki@live.dk>
2021-05-03 18:27:04 +0200

    --- Auto-Git Commit ---

--- laravel/resources/views/unit/index.blade.php
+++ laravel/resources/views/unit/index.blade.php
@@ -14,12 +14,10 @@ this is the index blade, this blade will be where everyone arrives
         <th>COMMAND</th>
     </tr>
 </table>
-<table class="indexinfo">
-    <tr><td>The 14th Air Assault Group is a unit based on United States Special Forces</td></tr>
-    <tr><td>our primary tasking consists of pure ground based assaults where out roster</td></tr>
-    <tr><td>is based around light vehicle transportation and air to surface support</td></tr>
-</table>
 
+<p class="aaginfo">The 14th Air Assault Group is a unit based on United States Special Forces
+our primary tasking consists of pure ground based assaults where out roster
+is based around light vehicle transportation and air to surface support</p>
 
 </div>
     
